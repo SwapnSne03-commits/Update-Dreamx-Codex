d6b31ca703473da6758dd27b4caa4e716c7cf2be
SwapnSne03-commits <somsneha03@gmail.com>
2026-04-23 13:58:58 +0530

    Update info.py
    Force subscribe remove

--- info.py
+++ info.py
@@ -56,8 +56,8 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002900228022')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Graduate_Request')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003878655942 -1003702769501")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1002612579654 -1003834489775")# Channels for force sub (make sure bot is admin)
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_channels     = environ.get("AUTH_CHANNELS", "")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
