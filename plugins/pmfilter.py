e5aeac158f2667f6e34351f5b7db533bdf2bf6af
SwapnSne03-commits <somsneha03@gmail.com>
2026-06-27 13:47:50 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2217,7 +2217,6 @@ async def auto_filter(client, msg, spoll=False):
                       # ]
                       # )
 
-        btn = add_smart_filter_buttons(btn, key)
         if offset != "":
             req = message.from_user.id if message.from_user else 0
             if ULTRA_FAST_MODE:
